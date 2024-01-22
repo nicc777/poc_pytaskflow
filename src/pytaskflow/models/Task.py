bcf481912465dea608f0fceb8d3a148df348a107
Nico Coetzee <nicc777@gmail.com>
2024-01-22 07:22:58 +0100

    added task functionality

--- src/pytaskflow/models/Task.py
+++ src/pytaskflow/models/Task.py
@@ -1,3 +1,6 @@
+import json
+import hashlib
+
 
 def keys_to_lower(data: dict):
     final_data = dict()
@@ -14,14 +17,23 @@ class Task:
     def __init__(self, kind: str, version: str, spec: dict, metadata: dict=dict()):
         self.kind = kind
         self.version = version
-        self.metadata = keys_to_lower(data=metadata)
-        self.spec = keys_to_lower(data=spec)
+        self.metadata = dict()
+        if metadata is not None:
+            if isinstance(metadata, dict):
+                self.metadata = keys_to_lower(data=metadata)
+        self.spec = dict()
+        if spec is not None:
+            if isinstance(spec, dict):
+                self.spec = keys_to_lower(data=spec)
         self.selector_register = dict()
         self.annotations = dict()
         self.task_dependencies = dict()
+        self.task_as_dict = dict()
         self._calculate_selector_registers()
         self._register_annotations()
         self._register_dependencies()
+        self.task_checksum = None
+        self.task_id = self._determine_task_id()
 
     def _calculate_selector_registers(self):
         if 'name' in self.metadata:
@@ -44,6 +56,27 @@ class Task:
                     if isinstance(context_dependencies, list):
                         self.task_dependencies[context] = context_dependencies
 
+    def _calculate_task_checksum(self)->str:
+        data = dict()
+        data['kind'] = self.kind
+        data['version'] = self.version
+        if len(self.metadata) > 0:
+            data['metadata'] = self.metadata
+        if len(self.spec) > 0:
+            data['spec'] = self.spec
+        self.task_as_dict = data
+        return hashlib.sha256(json.dumps(data).encode('utf-8')).hexdigest()
+
+    def _determine_task_id(self):
+        self.task_checksum = self._calculate_task_checksum()
+        if 'name' in self.selector_register:
+            self.task_id = hashlib.sha256(self.selector_register['name'].encode('utf-8')).hexdigest()
+        elif len(self.selector_register) > 0:
+            selector_register_json = json.dumps(self.selector_register)
+            self.task_id = hashlib.sha256(selector_register_json.encode('utf-8')).hexdigest()
+        else:
+            self.task_id = self.task_checksum
+
 
 class TaskProcessor:
 
@@ -58,12 +91,15 @@ class TaskProcessor:
 class Tasks:
 
     def __init__(self):
-        self.tasks = list()
+        self.tasks = dict()
         self.task_processors_executors = dict()
         self.task_processor_register = dict()
 
     def add_task(self, task: Task):
-        self.tasks.append(task)
+        processor_id = '{}:{}'.format(task.kind, task.version)
+        if processor_id not in self.task_processor_register:
+            raise Exception('Task kind "{}" with version "{}" has no processor registered. Ensure all task processors are registered before adding tasks.'.format(task.kind, task.version))
+        self.tasks[task.task_id] = task
 
     def register_task_processor(self, processor: TaskProcessor):
         if isinstance(processor.versions, list):
@@ -74,6 +110,27 @@ class Tasks:
             for version in processor.versions:
                 id = '{}:{}'.format(processor.kind, version)
                 self.task_processor_register[id] = executor_id
+
+    def process_context(self, command: str, context: str):
+        """
+            1. Determine the order based on task dependencies
+            2. Process tasks in order, with the available task processor registered for this task kind and version
+        """
+        task_order = list()
+        # TODO : order tasks
+
+        for task_id in task_order:
+            if task_id in self.tasks:
+                task = self.tasks[task_id]
+                target_task_processor_id = '{}:{}'.format(task.kind, task.version)
+                if target_task_processor_id in self.task_processor_register:
+                    target_task_processor_executor_id = self.task_processor_register[target_task_processor_id]
+                    if target_task_processor_executor_id in self.task_processors_executors:
+                        target_task_processor_executor = self.task_processors_executors[target_task_processor_executor_id]
+                        if isinstance(target_task_processor_executor, TaskProcessor):
+                            target_task_processor_executor.process_task(task=task, command=command, context=context)
+                    
+
 
     
 
